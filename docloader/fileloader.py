e752c83825c069e4e15100d1d1c772eaf7af6576
Paul Sushinski <pavel.sushi@gmail.com>
2015-04-27 23:03:39 +0300

    gettin dimensions

--- docloader/fileloader.py
+++ docloader/fileloader.py
@@ -3,10 +3,21 @@ import xlrd
 from prefs import UPLOADED_FILES_LOC as UFL
 
 
-def load_file(name):
-    rb = xlrd.open_workbook(UFL + name, formatting_info=True)
-    sheet = rb.sheet_by_index(0)
-    for row_num in range(sheet.nrows):
-        row = sheet.row_values(row_num)
-        for c_el in row:
-            print c_el
+class Workbook(object):
+    def __init__(self, filename):
+        self.workbook = xlrd.open_workbook(UFL + filename)
+        self.sheets = [self.workbook.sheet_by_index(i) for i in range(self.workbook.nsheets)]
+
+    def load_file(self):
+        sheet = self.workbook.sheet_by_index(0)
+        for row_num in range(sheet.nrows):
+            row = sheet.row_values(row_num)
+            for c_el in row:
+                print c_el
+
+    def dimensions(self):
+        res = [(sht.nrows, sht.ncols) for sht in self.sheets]
+        return res
+
+    def sheet_names(self):
+        return [name for name in self.workbook.sheet_names()]
--- docloader/tests/cases.py
+++ docloader/tests/cases.py
@@ -1,13 +1,23 @@
 __author__ = 'mac'
 import unittest
-from docloader import fileloader
+from docloader.fileloader import Workbook
 
 
 class LoadTester(unittest.TestCase):
 
     def test_loadfile(self):
         # todo check if file content uploaded to db
-        fileloader.load_file('test1.xls')
+        self.assertTrue(True)
+
+    def test_get_dimensions(self):
+        try:
+            wb = Workbook('test1.xls')
+            lz = zip(wb.sheet_names(), wb.dimensions())
+            print ','.join(['%s - (%d:%d)' % (l[0], l[1][0], l[1][1]) for l in lz])
+            self.assertTrue(True)
+        except Exception as ex:
+            print ex
+            self.assertTrue(False)
 
 
 if __name__ == '__main__':
